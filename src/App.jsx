4632a4f880296ab134f5fb0bbf98f2e4c5a9498a
niwayanrni <niwayanrinik@gmail.com>
2025-11-01 23:12:27 +0800

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
 import Login from './pages/login';
 import Register from './pages/register';
 import Home from './pages/home';
@@ -8,23 +8,41 @@ import MyEssays from './pages/essay/MyEssays';
 import SubmitEssay from "./pages/essay/SubmitEssay";
 import ViewGraded from "./pages/essay/ViewGraded";
 import InputEssay from "./pages/essay/InputEssay";
+import Navbar from "./pages/navbar";
 import './App.css';
+
+
+function MainLayout() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <div className="p-6">
+        <Outlet /> {/*  */}
+      </div>
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        {/* Halaman tanpa Navbar */}
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/my-course" element={<MyCourse />} />
-        <Route path="/course/:courseId" element={<CourseEssay />} />
-        <Route path="/my-essays" element={<MyEssays />} />
-        <Route path="/submit-essay" element={<SubmitEssay />} />
-        <Route path="/submit-essay/:courseId/:essayId" element={<InputEssay />} />
-        <Route path="/view-graded" element={<ViewGraded />} />
-        <Route path="/input-essay" element={<InputEssay />} />
+
+        {/* Halaman dengan Navbar */}
+        <Route element={<MainLayout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/my-course" element={<MyCourse />} />
+          <Route path="/course/:courseId" element={<CourseEssay />} />
+          <Route path="/my-essays" element={<MyEssays />} />
+          <Route path="/submit-essay" element={<SubmitEssay />} />
+          <Route path="/submit-essay/:courseId/:essayId" element={<InputEssay />} />
+          <Route path="/view-graded" element={<ViewGraded />} />
+          <Route path="/input-essay" element={<InputEssay />} />
+        </Route>
       </Routes>
     </Router>
   );
